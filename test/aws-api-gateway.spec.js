b55fc2b4eb62215a473b43af0578058d923ab585
John Kavanagh <gaudi@invisiblearchitects.com>
2019-11-13 12:45:29 +0000

    added lambda endpoint test

--- test/aws-api-gateway.spec.js
+++ test/aws-api-gateway.spec.js
@@ -1,25 +1,41 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const tags = require('mocha-tags');
-const { ApiGatewayUrl } = require('../config/aws.json');
+const fetch = require('node-fetch');
+const { ApiGatewayUrl, S3Bucket } = require('../config/aws.json');
 const auth = require('../src/auth');
 
 const should = chai.should();
 chai.use(chaiHttp);
 
+let response;
+
 tags('aws', 'auth', 'api')
-  .describe('cognito user auth process', () => {
+  .describe('AWS API', () => {
     it('is running', async () => {
 
-      const response = await chai
+      response = await chai
             .request(ApiGatewayUrl)
             .get('');
 
       response.should.have.property('status', 500)
     });
 
-    it('authenticates default user on default AWS gateway API', async () => {
-      const response = await auth;
+    it('responds with access token', async () => {
+      response = await auth;
       response.should.be.have.property('accessToken');
     });
+
+    it('allows access to lambda endpoint', async () => {
+      const { accessToken } = response;
+      const gateway = await fetch(ApiGatewayUrl+'/'+S3Bucket, {
+        method:'GET',
+        referrer: 'no-referrer',
+        headers: {
+          Authorization: accessToken,
+          "Access-Control-Request-Method": "GET",
+        }
+      });
+    })
+
   });
